cfffbbb9d4378f7665f7bc159ffff105b5c23fc9
ldzinl <reislf01587@gmail.com>
2026-05-04 20:04:18 -0300

    feature: sorteio pedido do dia

--- ByteBurger/src/main/java/org/example/Main.java
+++ ByteBurger/src/main/java/org/example/Main.java
@@ -4,14 +4,15 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        System.out.println("O item sorteado " +
+                "do dia e:" + itens[a].substring(3) +" com 20% de desconto!");
+        exibirMenu();
+        while (!scanner.hasNextInt()) {
+            System.out.println("Opcao Invalida");
+            scanner.next();
         }
+        opcaoCliente = scanner.nextInt();
+        scanner.nextLine();
+        break;
     }
 }
