4dc7084ceb566c58481e5df54135ed749307d4e8
eltonsaes <eltonsaes24@gmail.com>
2026-05-04 20:12:59 -0300

    troco

--- ByteBurger/src/main/java/org/example/Main.java
+++ ByteBurger/src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    static void main(String[] args) {
+        double valorRecebido;
+        double valorTotalPedido = 0;
+
+        System.out.println("Digite o valor possuido:");
+        valorRecebido = sc.nextDouble();
+
+        while(valorRecebido < valorTotalPedido){
+            System.out.println("Valor " +
+                    "Insuficiente");
+            System.out.println("Adicioe mais " +
+                    "fundos");
+
+            System.out.println("Valor total do pedido: " + valorTotalPedido);
+            valorRecebido += sc.nextDouble();
         }
+        double troco = valorRecebido - valorTotalPedido;
+        System.out.println("Troco e: " + troco);
+
+        }
+
     }
 }
