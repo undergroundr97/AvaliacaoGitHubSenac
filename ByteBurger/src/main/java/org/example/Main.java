4f1dc1362d72e74b1f62d936c151e52c7642d737
Elton Henrique <eltonsaes24@gmail.com>
2026-05-07 17:49:46 -0300

    troco adicionado

--- ByteBurger/src/main/java/org/example/Main.java
+++ ByteBurger/src/main/java/org/example/Main.java
@@ -111,6 +111,22 @@ public class Main {
                 case 2:
                     break;
                 case 3:
+                    double valorRecebido;
+                    double valorTotalPedido = 0;
+
+                    System.out.println("Digite o valor possuido:");
+                    valorRecebido = sc.nextDouble();
+
+                    while(valorRecebido < valorTotalPedido){
+                    System.out.println("Valor " + "Insuficiente");
+                    System.out.println("Adicioe mais " + "fundos");
+
+                    System.out.println("Valor total do pedido: " + valorTotalPedido);
+                    valorRecebido += sc.nextDouble();
+                    }
+                    double troco = valorRecebido - valorTotalPedido;
+                    System.out.println("Troco e: " + troco);
+
                     break;
                 case 4:
                     System.out.println("O item sorteado " +
